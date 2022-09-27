076043d8f0d26f177fc42202a94eb47beb8296f3
Daniel <alum.dveranom.2021@iesalixar.org>
2022-09-27 08:29:04 +0200

    Javascript Repaso 1

---
+++ 2-Javascript/app.js
@@ -0,0 +1,7 @@
+alert('Hello world')
+console.log('Log message')
+console.info('Info message')
+console.error('Error message')
+console.warn('Warning message')
+debugger
+console.log('After debugger');
